ea8958b93aac4a62e50e4e53224664ac19c27678
Bosaidonio <Bosaidonio@163.com>
2025-05-11 12:56:04 +0800

    feat: 添加 robots.txt 和 sitemap 配置文件
    添加 robots.txt 和 sitemap.xml 文件，并配置 next-sitemap 以自动生成站点地图。更新 package.json 以集成 next-sitemap 并在构建后生成站点地图。

---
+++ next-sitemap.config.js
@@ -0,0 +1,20 @@
+/** @type {import('next-sitemap').IConfig} */
+const config =  {
+  siteUrl: process.env.SITE_URL || 'http://localhost:3000',
+  generateRobotsTxt: true, // 生成 robots.txt 文件
+  sitemapSize: 7000, // 每个站点地图的最大 URL 数
+  changefreq: 'daily', // 更改频率
+  priority: 0.7, // 优先级
+  exclude: ['/server-sitemap.xml'], // 排除的路径
+  robotsTxtOptions: {
+    policies: [
+      {
+        // 允许所有用户访问所有页面
+        userAgent: '*',
+        // 允许所有页面
+        allow: '/',
+      },
+    ],
+  },
+}
+export default config
