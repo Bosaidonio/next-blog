a1f0a5ac7415c3af8df2ef907e87f1497b9b3d85
Bosaidonio <Bosaidonio@163.com>
2025-05-11 13:17:02 +0800

    fix(middleware): 修正sitemap文件的匹配规则
    修正了matcher配置中sitemap文件的匹配规则，确保sitemap-[0-9]+.xml文件被正确忽略

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,6 +3,6 @@ export { middleware } from 'nextra/locales'
 export const config = {
   // Matcher ignoring `/_next/` and `/api/`
   matcher: [
-    '/((?!api|_next/static|_next/image|assets|favicon.ico|icon.svg|apple-icon.png|manifest|_pagefind|sitemap.xml|sitemap-*.xml|robots.txt).*)'
+    '/((?!api|_next/static|_next/image|assets|favicon.ico|icon.svg|apple-icon.png|manifest|_pagefind|sitemap.xml|sitemap-[0-9]+.xml|robots.txt).*)'
   ]
 }
