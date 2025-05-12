a25ad2b4c2e697476d734f9f85456a8b1da5d7c3
Bosaidonio <Bosaidonio@163.com>
2025-05-12 09:03:43 +0800

    test: 测试

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next-sitemap').IConfig} */
 const config =  {
   siteUrl: process.env.SITE_URL || 'http://localhost:3000',
+  generateIndexSitemap: false, // 不生成索引站点地图
   generateRobotsTxt: true, // 生成 robots.txt 文件
   sitemapSize: 7000, // 每个站点地图的最大 URL 数
   changefreq: 'daily', // 更改频率
   priority: 0.7, // 优先级
-  exclude: ['/server-sitemap.xml'], // 排除的路径
   robotsTxtOptions: {
     policies: [
       {
